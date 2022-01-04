d91306035aca11da3e98e99aff1ed8b8589106a0
myroslav <myrkozar@gmail.com>
2022-01-04 20:13:22 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+
+const userName = "myroslav";
+const age = 35;
+console.log(age);
+console.log(userName);
+let secondName = "leon";
+console.log(secondName);
+secondName = "ciller";
+console.log(secondName);
+let thirdName;
+console.log(thirdName);
+console.log(typeof userName);
+console.log(typeof age);
+const hello = false;
+let world = null;
+console.log(typeof hello);
+console.log(typeof world);
+console.log('username is', userName);
+
+// const isComing = confirm('please enter');
+// console.log(isComing);
+// const hotelName = prompt('please enter');
+// console.log(hotelName);
+
+// const value = prompt('enter nuber');
+// console.log(typeof value);
+// console.log(value);
+
+
+const x = 2;
+const y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(y / x);
+console.log(y % x);
