9b45728098e1c196df1e97b9c40493d042db10f7
myroslav <myrkozar@gmail.com>
2022-02-12 13:05:41 +0200

    jm

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 "use strict";
 
 
-const userName = "myroslav";
-const age = 35;
-console.log(age);
-console.log(userName);
-let secondName = "leon";
-console.log(secondName);
-secondName = "ciller";
-console.log(secondName);
-let thirdName;
-console.log(thirdName);
-console.log(typeof userName);
-console.log(typeof age);
-const hello = false;
-let world = null;
-console.log(typeof hello);
-console.log(typeof world);
-console.log('username is', userName);
+// const userName = "myroslav";
+// const age = 35;
+// console.log(age);
+// console.log(userName);
+// let secondName = "leon";
+// console.log(secondName);
+// secondName = "ciller";
+// console.log(secondName);
+// let thirdName;
+// console.log(thirdName);
+// console.log(typeof userName);
+// console.log(typeof age);
+// const hello = false;
+// let world = null;
+// console.log(typeof hello);
+// console.log(typeof world);
+// console.log('username is', userName);
 
 // const isComing = confirm('please enter');
 // console.log(isComing);
@@ -29,53 +29,235 @@ console.log('username is', userName);
 // console.log(value);
 
 
-const x = 2;
-const y = 3;
+// const x = 2;
+// const y = 3;
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(y / x);
-console.log(y % x);
-
-
-const aNumber = '5px';
-console.log(Number(aNumber));
-console.log(Number.parseInt(aNumber));
-let frog = '13.22vrl';
-console.log(Number.parseFloat(frog));
-frog = Number('51');
-console.log(Number.isNaN(frog));
-frog = Number('jnlkml');
-console.log(Number.isNaN(frog));
-console.log(0.1 + 0.2);
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(y / x);
+// console.log(y % x);
 
 
-console.log(0.32 + 0.54);
-console.log((0.75 + 0.15).toFixed(4));
-console.log(Math.floor(1.7));
-console.log(Math.ceil(4.4));
-console.log(Math.max(20, 30, 40, 50, 120, 40,));
-console.log(Math.min(10, 35, 5, 44));
-console.log(Math.pow(3, 3));
-console.log(Math.random());
-console.log(Math.random());
-console.log(Math.random() * (10 - 1) + 1);
+// const aNumber = '5px';
+// console.log(Number(aNumber));
+// console.log(Number.parseInt(aNumber));
+// let frog = '13.22vrl';
+// console.log(Number.parseFloat(frog));
+// frog = Number('51');
+// console.log(Number.isNaN(frog));
+// frog = Number('jnlkml');
+// console.log(Number.isNaN(frog));
+// console.log(0.1 + 0.2);
 
 
-let ses = 'happy new yars';
-console.log(ses.length);
+// console.log(0.32 + 0.54);
+// console.log((0.75 + 0.15).toFixed(4));
+// console.log(Math.floor(1.7));
+// console.log(Math.ceil(4.4));
+// console.log(Math.max(20, 30, 40, 50, 120, 40,));
+// console.log(Math.min(10, 35, 5, 44));
+// console.log(Math.pow(3, 3));
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random() * (10 - 1) + 1);
 
-console.log(ses.toLocaleLowerCase());
-console.log(ses.toLocaleUpperCase());
-console.log(ses.indexOf('new'));
-console.log(ses.indexOf('r'));
-console.log(ses.indexOf('y'));
-console.log(ses.endsWith('rs'));
-console.log(ses.endsWith('ew'));
+
+// let ses = 'happy new yars';
+// console.log(ses.length);
+
+// console.log(ses.toLocaleLowerCase());
+// console.log(ses.toLocaleUpperCase());
+// console.log(ses.indexOf('new'));
+// console.log(ses.indexOf('r'));
+// console.log(ses.indexOf('y'));
+// console.log(ses.endsWith('rs'));
+// console.log(ses.endsWith('ew'));
 
 
-const ageMine = 36;
-console.log(ageMine);
-console.log(ageMine > 33 && ageMine < 40);
-console.log(ageMine > 32 && ageMine > 40);
+// const ageMine = 36;
+// console.log(ageMine);
+// console.log(ageMine > 33 && ageMine < 40);
+// console.log(ageMine > 32 && ageMine > 40);
+
+
+
+// const q = 'g';
+// let w = 0;
+// if (q === 'g'){
+//     w = 4;
+// }
+// console.log(w);
+
+// const q = 'x';
+// let w;
+// if (q === 'x'){
+//     w = 4;
+// }
+//     else{
+//         w = 2;
+//     }
+// console.log(w);    
+
+
+// const q = 'x';
+// let w;
+// if (q === 'w'){
+//     w = 0;
+// }
+//     else if (q === 'e') {
+//         w = 1;
+//     }
+//     else if (q === 'r'){
+//         w = 2;
+//     }
+//     else if (q === 'x'){
+//         w = '100';
+//     }
+//     else{
+//         console.log('fuck');
+//     }
+// console.log(w);
+
+// const q = 35;
+// let userName;
+// if (q >= 36){
+//     userName = 'muroslav';
+// }
+// else {
+//     userName = 'notMe';
+// }
+// console.log(userName);
+
+// const userName = q >= 36 ? 'muroslav' : 'notMe';
+// console.log(userName);
+
+
+// const x = 1;
+// const y = 2;
+// let bigger;
+// if (x > y){
+//     bigger = x;
+// }
+// else {
+//     bigger = y;
+// }
+// console.log(bigger);
+
+// const q = 45;
+// const w = 409;
+// if (q > w){
+//     bigger = q; 
+// }
+// else {
+//     bigger = w;
+// }
+// console.log(bigger);
+// const a = 24;
+// const s = '45';
+// if ( a > s){
+//     bigger = a;
+// }
+// else{
+//     bigger = s;
+// }
+// console.log(bigger);
+
+// const bigger = x > y ? x : y;
+// console.log(bigger);
+// const bigger = q > w ? q : w;
+// console.log(bigger);
+// const bigger = a > s ? a : s;
+// console.log(bigger);
+
+
+// let q;
+// const w = 'yes';
+// switch (w){
+//     case 'j':
+//         q = 2;
+//         break;
+//     case 'f':
+//         q = 3;
+//         break;
+//     case 'yes':
+//         q = 'no';
+//         break;
+//     default:
+//         console.log('fuck');            
+// }
+// console.log(q);
+
+
+
+// const employees = 10;
+// const maxSalary = 5000;
+// const minSalary = 500;
+// let totalSalary = 0;
+// for (let i = 1; i <= employees; i += 1 ){
+//     const salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+//     console.log(`зп раб n ${i} - ${salary}`);
+
+//     totalSalary += salary;
+// }
+// console.log('total: ', totalSalary);
+
+
+
+// const stars = 2;
+// let price ;
+
+// if(stars === 1){
+//     price = 20;
+// } else if(stars === 2) {
+//     price = 50;
+// } else if(stars === 3) {
+//     price = 60;
+// } else {
+//     console.log('fuck');
+// }
+// switch(stars){
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 50;
+//         break;
+//     case 3:
+//         price = 60;
+//         break;  
+    
+//         default:
+//             console.log('fuck');
+// }
+
+// console.log(price);
+
+
+
+// const option = 4;
+// let message = '';
+
+// switch(option){
+//     case 1:
+//         message = 'q';
+//         break;
+//     case 2:
+//         message = 'w';
+//         break;
+//     case 3:
+//         message = 'e';
+//         break;
+//     default:
+//         message = 'fuck';                
+        
+// }
+// console.log(message);   
+
+
+// let counter = 0;
+//  while (counter < 10){
+//      console.log('counter: ', counter);
+//      counter += 1;
+//  }
+
