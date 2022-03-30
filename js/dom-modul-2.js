7442fa92567ec4a6ae85bce8aa9eeb1e7dd1589e
myroslav <myrkozar@gmail.com>
2022-03-30 22:19:17 +0300

    last

---
+++ js/dom-modul-2.js
@@ -0,0 +1,59 @@
+'use strict'
+
+// const newElDiv = document.createElement('div')
+// newElDiv.classList.add('btnForTestEv')
+// newElDiv.style.padding = '10px'
+// newElDiv.style.backgroundColor = 'blue'
+
+// console.log(newElDiv);
+// const nameOfBtn = [{class: 'js-target-btn', txt: 'кнопка'}, {class: 'js-add-listener', txt: 'добавить слушателя'}, {class: 'js-remove-listener', txt: 'снять слушателя'}]
+
+// const newListBtn = arr => arr.map(option => {
+//     const buttonNew = document.createElement('button')
+//     buttonNew.type = 'button'
+//     buttonNew.classList = option.class
+//     buttonNew.textContent = option.txt
+//     buttonNew.style.margin = '10px'
+    
+
+//     return buttonNew
+// })
+// // console.log(newListBtn(nameOfBtn));
+// const makeContainerWithBtn = newListBtn(nameOfBtn)
+// newElDiv.append(...makeContainerWithBtn)
+
+// document.body.append(newElDiv)
+
+// const firstBtn = document.querySelector('.js-target-btn')
+// const secondBtn = document.querySelector('.js-add-listener')
+// const thirdBtn = document.querySelector('.js-remove-listener')
+
+// firstBtn.addEventListener('click', handleTargetButtonClick
+//    )
+
+//function handleTargetButtonClick() {
+//    console.log('клик');
+// }
+//targetButtonClickHandler
+// onTargetButtonClick
+
+// secondBtn.addEventListener('click', () => {
+//     console.log('вешаю слушателя на кнопка');
+
+//     firstBtn.addEventListener('click', onTargetButtonClick)
+// })
+
+// thirdBtn.addEventListener('click', () => {
+//     console.log('снимаю слушателя на кнопка');
+
+//     firstBtn.removeEventListener('click', onTargetButtonClick)
+// })
+
+// function onTargetButtonClick() {
+//     console.log('клик по кнопке');
+// }
+
+// function logMassage(){
+//     console.log('клик по целевой кнопке');
+// }
+// console.log();
