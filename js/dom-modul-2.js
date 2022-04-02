c7a55df558b4d2c65ac8d840a8ee6dbe21763c24
myroslav <myrkozar@gmail.com>
2022-04-02 10:41:14 +0300

    last

--- js/dom-modul-2.js
+++ js/dom-modul-2.js
@@ -208,3 +208,41 @@
 // function onClearOutput() {
 //     refs1.output.textContent = ''
 // }
+
+////////////////////////////////////////////////
+
+const refs = {
+    openModalBtn: document.querySelector('[data-action="open-modal"]'),
+    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+    backdrop: document.querySelector('.js-backdrop')
+}
+
+console.log(refs);
+refs.openModalBtn.addEventListener('click', onOpenModal)
+refs.closeModalBtn.addEventListener('click', onCloseModal)
+refs.backdrop.addEventListener('click', onBackdropClick)
+
+function onOpenModal() {
+    window.addEventListener('keydown', onEscKeyPress)
+    document.body.classList.add('show-modal')
+}
+
+function onCloseModal () {
+    window.removeEventListener('keydown', onEscKeyPress)
+document.body.classList.remove('show-modal')
+}
+
+function onBackdropClick (event) {
+    
+    if(event.currentTarget === event.target){
+        onCloseModal()
+    }
+}
+
+function onEscKeyPress (event) {
+const ESC_KEY_CODE = 'Escape'
+
+    if(event.code === ESC_KEY_CODE){
+        onCloseModal()
+    }
+}
